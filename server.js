31016574e2d9445e3e6f9ee217d025738fe8c2eb
BreezeLA <109669332+BreezeLA@users.noreply.github.com>
2023-03-27 13:41:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -140,16 +140,15 @@ async function consturctServer(moduleDefs) {
    * CORS & Preflight request
    */
   app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'),
     if (req.path !== '/' && !req.path.includes('.')) {
-      res.header('Access-Control-Allow-Origin', '*'),
-//       res.set({
-//         'Access-Control-Allow-Credentials': true,
-//         'Access-Control-Allow-Origin':
-//           CORS_ALLOW_ORIGIN || req.headers.origin || '*',
-//         'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
-//         'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
-//         'Content-Type': 'application/json; charset=utf-8',
-//       })
+      res.set({
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': CORS_ALLOW_ORIGIN || req.headers.origin || '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+        'Content-Type': 'application/json; charset=utf-8',
+      })
     }
     req.method === 'OPTIONS' ? res.status(204).end() : next()
   })
